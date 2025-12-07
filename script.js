44099a4048fb4e662511f532cabd0978d9a4fcf2
Apif Supriadi <apif.supriadi@uiii.ac.id>
2025-12-07 16:49:11 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+lucide.createIcons();
+/* 
+    Penggunaan lucide iconnya
+    <i data-lucide="heart" class="w-6 h-6 text-red-500"></i>
+    <i data-lucide="star" class="w-8 h-8 text-yellow-400"></i>
+    <i data-lucide="shopping-cart" class="w-10 h-10 text-blue-600"></i>
+*/
